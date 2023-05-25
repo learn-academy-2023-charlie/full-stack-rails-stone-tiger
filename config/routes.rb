559593254cc5bdde0ab30b63c607e792765887ce
surielisrodriguez <surielisrodriguez7@gmail.com>
2023-05-25 11:37:01 -0700

    added blog index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "blog#index"
+  # get '/blogs' => 'blog#index'
+  get '/blogs', to: 'blog#index'
 end
